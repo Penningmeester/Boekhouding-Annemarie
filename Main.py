e4f7f8a8c5164ebc50053b5f044d7b2fa339b29d
penningmeester <pjjmeewis@gmail.com>
2018-05-10 12:55:06 +0200

    Created the Main file for assignment 2
    Main file with essential packages imported.

---
+++ Main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main.py
+
+Purpose:
+    Assignment 2 of Data Mining course from the Vrije Universiteit Amsterdam
+
+Date:
+    2018/05/09
+
+@author: 	Pepijn Meewis
+			Tijn Wijdoogen
+			Winsen Duker
+"""
+###########################################################
+### Imports
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+# import scipy.optimize as opt
+# from pandas_datareader import data as web
+
+#==============================================================================
+# Import data
+#==============================================================================
+
+
+
+
+###########################################################
+### main
+def main():
+    # Magic numbers
+    
+
+    # Initialisation
+    # Output
+
+
+###########################################################
+### start main
+if __name__ == "__main__":
+    main()
