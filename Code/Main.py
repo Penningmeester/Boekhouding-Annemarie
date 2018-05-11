415984424556425d0521bb82308f3c522db6be62
penningmeester <pjjmeewis@gmail.com>
2018-05-11 16:26:02 +0200

    added machine learning packages to the main file
    Imported machine learning packages into the main file

--- Code/Main.py
+++ Code/Main.py
@@ -28,7 +28,16 @@ import pandas as pd
 # import scipy.optimize as opt
 # from pandas_datareader import data as web
 
-
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 
 #==============================================================================
 # Import data
