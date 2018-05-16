3e022a7faa138388563ad84d03e54918e81b630b
WinsenDuker <39156203+WinsenDuker@users.noreply.github.com>
2018-05-17 00:49:55 +0200

    Completed Xu's code. Not fully adjusted
    Still need to adjust code and analyse properly. Do not understand creation of certain features yet.

--- Code/User_explorer.py
+++ Code/User_explorer.py
@@ -144,7 +144,94 @@ def User_explorer(train):
     # Result: If booked before, more likely to book again. No differnence on clicking
     # As value of historical booking has no impact, change NaNs to zeros
     
+## Investigate Property Review score
+    nanrate(train, "prop_review_score")
+    ratedist(train, "prop_review_score", 0.5, -1)
+    # Result: If a property is rated, its more likely to be clicked and booked. Especially if rating is between 6-9
+    # To smooth curve, Xu sets 0.0 (no reviews) to 2.5 and NaN to 3.0. 
+    train['prop_review_score'].fillna(3, inplace=True)
+    train['prop_review_score'][train['prop_review_score']==0]=2.5
+    ratedist(train, "prop_review_score", 0.5, -1)
+    # Do not fully agree with this method as it's clear that score between 6-9 are more likely to be clicked and booked
 
+## Investigate Property Location score2
+    nanrate(train, "prop_location_score2")
+    train["prop_location_score2"].describe()
+    ratedist(train, "prop_location_score2", 0.1, -0.1)
+    # Result: Replace NA with zeros
+    # No clue why only look at score2, why not score1 first?
+
+## Investigate Distance user and location
+    nanrate(train, "orig_destination_distance")
+    train["orig_destination_distance"].describe()
+    # Take log
+    train["orig_destination_distance_log"] = np.log(train["orig_destination_distance"])
+    train["orig_destination_distance_log"].describe()
+    ratedist(train, "orig_destination_distance_log", 1, -6)
+    # Replace NAs with 75% value/quantile 
+    train["orig_destination_distance"].fillna(1509,inplace=True)
+    train["orig_destination_distance_log"] = np.log(train["orig_destination_distance"])
+    ratedist(train, "orig_destination_distance_log", 1, -6)
+    # Cant see the logic/impact on booking
+
+## Investigate Propability to be clicked feature
+    nanrate(train, "srch_query_affinity_score")
+    train["srch_query_affinity_score"].describe()
+    # Booking rate is higher for properties with a score
+    train["srch_query_affinity_score_log"] = np.log(-train["srch_query_affinity_score"])
+    train["srch_query_affinity_score_log"].describe()
+    ratedist(train, "srch_query_affinity_score_log", 0.5, 0)
+    # Result: Replace NAs with mean value
+
+## Investigate Competitor info
+    nanrate(train, "comp5_rate")
+    print(train["comp5_rate"].describe())
+    ratedist(train, "comp5_rate", 1, -2)
+    
+    nanrate(train, "comp5_inv")
+    print(train["comp5_inv"].describe())
+    ratedist(train, "comp5_inv", 1, -2)
+    
+    ratedist(train, "comp2_inv", 1, -2)
+    ratedist(train, "comp8_inv", 1, -2)
+    ratedist(train, "comp3_inv", 1, -2)
+    
+    # Result: Comp_rate influences click and booking, comp_inv is negatively w.r.t. book/click
+    # comp5_rate_percent_diff seems to be useless, still need to analyse
+    # Add comp_i_rate and inverse to get comp_sum
+    # All NAs to zero
+
+    for i in range(1,9):
+        train['comp'+str(i)+'_inv'].fillna(0, inplace=True)
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==1] = 10
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==-1] = 1
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==0] = -1
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==10] = 0
+        train['comp_inv_sum'] = train['comp1_inv']
+        for i in range(2,9):
+            train['comp_inv_sum'] += train['comp'+str(i)+'_inv']
+        train['comp_inv_sum'].describe()
+            
+        ratedist(train, "comp_inv_sum", 1, -9)
+
+    for i in range(1,9):
+        train['comp'+str(i)+'_rate'].fillna(0, inplace=True)
+    train['comp_rate_sum'] = train['comp1_rate']
+    for i in range(2,9):
+        train['comp_rate_sum'] += train['comp'+str(i)+'_rate']
+    train['comp_rate_sum'].describe()
+    
+    ratedist(train, "comp_rate_sum", 1, -7)
+
+    train['comp_sum'] = train['comp1_rate'] + train['comp1_inv'] + 2
+    for i in range(2,9):
+        train['comp_sum'] += train['comp'+str(i)+'_rate'] + train['comp'+str(i)+'_inv']    
+    train["comp_sum"].describe()    
+    ratedist(train, "comp_sum", 1, -13)
+
+    for i in range(1,9):
+        ratedist(train, 'comp'+str(i)+'_rate', 1, -2)
+    #Result: Remove all comp info and create features comp_rate_sum comp_rate_sum?
 
     
     print('Data is explored')
