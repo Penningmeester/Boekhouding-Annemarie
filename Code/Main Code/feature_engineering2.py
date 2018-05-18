6059591e9a0330f779d4149f55af53927413ab3e
WinsenDuker <39156203+WinsenDuker@users.noreply.github.com>
2018-05-18 16:50:34 +0200

    Added feature engineering

--- Code/Main Code/feature_engineering2.py
+++ Code/Main Code/feature_engineering2.py
@@ -26,20 +26,55 @@ def get_features(train, isBook=True):
 
 
 def feature_eng(train):
-
     # deal with NAs in hotels's infor
     train['prop_review_score'].fillna(0, inplace=True)
-    
-    train["prop_location_score2"].fillna(0, inplace=True)
-    
-    avg_srch_score = train["srch_query_affinity_score"].mean()
-    train["srch_query_affinity_score"].fillna(avg_srch_score, inplace=True)
-    train["orig_destination_distance"].fillna(1509,inplace=True)
-    train["visitor_hist_adr_usd"].fillna(0, inplace=True)
-    train['visitor_hist_starrating_bool'] = pd.notnull(train['visitor_hist_starrating'])
+        
     train['Price_pp_usd'] = train['price_usd'] / (train['srch_adults_count'] + train['srch_children_count'])
 
+    # Winsen
+    vist_hist_rating = vist_hist_rating.fillna(0, inplace=True)
+    train['visitor_hist_starrating_bool'] = train["visitor_hist_starrating"].notnull()
+    train["visitor_hist_adr_usd"].fillna(0, inplace=True)
+    train['visitor_hist_adr_usd_bool'] = vist_hist_spend.notnull()
+    prop_rev_score.fillna(3, inplace=True)
+    train['prop_review_score'][train['prop_review_score']==0]=2.5
+    train["prop_location_score2"].fillna(0, inplace=True)
+    prop_location1.fillna(0, inplace=True)
+    prop_log_histprice.fillna(prop_log_histprice.mean(),inplace=True)
+    train["orig_destination_distance_log"] = np.log(orig_dest_dist)
+    srch_query_score.fillna(srch_query_score.mean(),inplace=True)
+
+
+    train['comp_sum'] = train['comp1_rate'] + train['comp1_inv'] + 2
+    for i in range(2,9):
+        train['comp_sum'] += train['comp'+str(i)+'_rate'] + train['comp'+str(i)+'_inv']    
+    train["comp_sum"].describe()    
+    ratedist(train, "comp_sum", 1, -13)
+
+    for i in range(1,9):
+        ratedist(train, 'comp'+str(i)+'_rate', 1, -2)
+
+    for i in range(1,9):
+        train['comp'+str(i)+'_inv'].fillna(0, inplace=True)
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==1] = 2
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==-1] = 1
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==0] = -1
+        train['comp'+str(i)+'_inv'][train['comp'+str(i)+'_inv']==2] = 0
+        train['comp_inv_sum'] = train['comp1_inv']
+        for i in range(2,9):
+            train['comp_inv_sum'] += train['comp'+str(i)+'_inv']
+        train['comp_inv_sum'].describe()
+            
+        ratedist(train, "comp_inv_sum", 1, -9)
+
+    for i in range(1,9):
+        train['comp'+str(i)+'_rate'].fillna(0, inplace=True)
+    train['comp_rate_sum'] = train['comp1_rate']
+    for i in range(2,9):
+        train['comp_rate_sum'] += train['comp'+str(i)+'_rate']
+    train['comp_rate_sum'].describe()
     
+    ratedist(train, "comp_rate_sum", 1, -7)
 
 
 def main():
